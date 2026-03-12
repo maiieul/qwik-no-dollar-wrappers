e1cc6d88769a7de85634a3f2f43e3d8a9a897238
maiieul <45822175+maiieul@users.noreply.github.com>
2026-03-12 10:52:31 +0100

    chore: work with external factory

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,37 +1,18 @@
-import {
-  $,
-  component$,
-  noSerialize,
-  NoSerialize,
-  Signal,
-  useSignal,
-} from "@qwik.dev/core";
+import { $, component$, Signal, useSignal } from "@qwik.dev/core";
+
+const someFactory = (instance: number, count: Signal<number>) => {
+  let timesCalled = 0;
+
+  return function () {
+    timesCalled++;
+    count.value = count.value + instance + timesCalled;
+  };
+};
 
 export default component$(() => {
   const count = useSignal(0);
-  const instance = useSignal(123);
-  const incrementFn = useSignal<() => void | undefined>();
 
-  const someFactory = $((instance: number, count: Signal<number>) => {
-    let timesCalled = 0;
+  const incrementFn = $(() => someFactory(123, count)());
 
-    return function () {
-      timesCalled++;
-      count.value = count.value + instance + timesCalled;
-    };
-  });
-
-  return (
-    <button
-      onClick$={async () => {
-        if (!incrementFn.value) {
-          incrementFn.value = await someFactory(instance.value, count);
-        }
-
-        incrementFn.value?.();
-      }}
-    >
-      {count.value}
-    </button>
-  );
+  return <button onClick$={incrementFn}>{count.value}</button>;
 });
