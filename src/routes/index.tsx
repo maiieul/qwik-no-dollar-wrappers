223df8772d565cb7b8665765eb1e36fbe2db5bbe
maiieul <45822175+maiieul@users.noreply.github.com>
2026-03-12 11:11:01 +0100

    chore: using a class

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,22 @@
 import { $, component$, Signal, useSignal } from "@qwik.dev/core";
 
-const someFactory = (instance: number, count: Signal<number>) => {
-  let timesCalled = 0;
+const MyClass = class {
+  constructor(public readonly instance: number) {}
+};
 
+const someFactory = (instance: number, count: Signal<number>) => {
   return function () {
-    timesCalled++;
-    count.value = count.value + instance + timesCalled;
+    count.value = count.value + instance;
   };
 };
 
 export default component$(() => {
   const count = useSignal(0);
 
-  const incrementFn = $(() => someFactory(123, count)());
+  const incrementFn = $(() => {
+    const myClass = new MyClass(123);
+    return someFactory(myClass.instance, count)();
+  });
 
   return <button onClick$={incrementFn}>{count.value}</button>;
 });
